e59dea24ab9026d26ffa0d2113090ea9a8fe4f78
Sadharan Ladka <debdutbhatta2007@gmail.com>
2024-07-28 08:11:47 +0530

    update

---
+++ questions.js
@@ -0,0 +1,410 @@
+let questions = [
+    {
+        numb: 1,
+        question: "",
+        answer: "",
+        options: [
+            
+        ]
+    },
+    {
+        numb: 2,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 3,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 4,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 5,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 6,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 7,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 8,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 9,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 10,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 11,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 12,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 13,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 14,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 15,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 16,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 17,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 18,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 19,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 20,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 21,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 22,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 23,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 24,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 25,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 26,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 27,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 28,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 29,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 30,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 31,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 32,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 33,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 34,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 35,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 36,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 37,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 38,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 39,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 40,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 41,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 42,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 43,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 44,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 45,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 46,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 47,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 48,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 49,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 50,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+    {
+        numb: 51,
+        question: "",
+        answer: "",
+        options: [
+
+        ]
+    },
+];
